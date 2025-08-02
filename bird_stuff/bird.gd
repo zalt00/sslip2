9db95fdbfaa823bc35a2e17f57fd2ac20f2b8acf
Emile C. <docteur.raton@gmail.com>
2025-08-02 22:54:34 +0200

    Adds a kill animation

--- bird_stuff/bird.gd
+++ bird_stuff/bird.gd
@@ -3,11 +3,21 @@ extends CharacterBody2D
 @onready var chick_scene = preload("res://bird_stuff/chick.tscn")
 #@onready var collision_polygon_2d: CollisionPolygon2D = $Area2D/CollisionPolygon2D
 
+class Chick:
+	var node: Node2D
+	var virtual_position: Vector2
+	var interpolation_factor: float
+	func _init(n, v, i):
+		node = n
+		virtual_position = v
+		interpolation_factor = i
+
 var num_positions
 var previous_positions = []
 var num_chicks = 30
 var chick_interval = 5
-var chicks = []
+var chicks: Array = []
+var time_since_last_loop = 0
 
 func _ready() -> void:
 	num_positions = (num_chicks + 1) * chick_interval
@@ -15,29 +25,46 @@ func _ready() -> void:
 		previous_positions.append(position)
 	for i in range(num_chicks):
 		var chick_cur = chick_scene.instantiate()
-		chicks.append(chick_cur)
+		chicks.append(Chick.new(chick_cur, Vector2.ZERO, 10.))
 		add_sibling.call_deferred(chick_cur)
-		chick_cur.position = position + 100. * Vector2.DOWN
 
 func _process(delta: float) -> void:
 	previous_positions.pop_back()
 	previous_positions.push_front(position)
 	for i in range(num_chicks):
-		var c = chicks[i]
-		c.position = previous_positions[chick_interval * (i + 1)]
-		c.position.y += 15. * ((1 + i) ** .1) * sin(.005 * Time.get_ticks_msec() + .5 * i)
-		var hash = i * 49824.4975
-		hash = hash - int(hash)
-		hash *= TAU
-		c.position.y += 5. * sin(.02 * Time.get_ticks_msec() + hash)
+		chicks[i].virtual_position = previous_positions[chick_interval * (i + 1)]
+		chicks[i].virtual_position.y += 15. * ((1 + i) ** .1) * sin(.005 * Time.get_ticks_msec() + .5 * i)
+		var h = i * 49824.4975
+		h = h - int(h)
+		h *= TAU
+		chicks[i].virtual_position.y += 5. * sin(.02 * Time.get_ticks_msec() + h)
 	$Area2D/CollisionShape2D.shape.points = []
+	time_since_last_loop += delta
+	var loop_found = false
 	for i in range(num_chicks):
 		for j in range(i + 5, num_chicks):
-			if (chicks[j].position - chicks[i].position).length() < 40.:
+			if (chicks[j].node.position - chicks[i].node.position).length() < 40.:
+				loop_found = true
+				if time_since_last_loop >= 1.:
+					time_since_last_loop = 0.
+					if (j - i + 1) % 2 == 1:
+						--j
+					for k in range(i, (i + j) / 2):
+						var tmp = chicks[k].node.position
+						chicks[k].interpolation_factor = 1.
+						chicks[k + (j - i + 1) / 2].interpolation_factor = 1.
+						chicks[k].node.position = chicks[k + (j - i + 1) / 2].node.position
+						chicks[k + (j - i + 1) / 2].node.position = tmp
 				var polygon: PackedVector2Array = []
-				for k in range(i, j - 1):
-					polygon.append(to_local(chicks[k].position))
+				for k in range(i, j + 1):
+					polygon.append(to_local(chicks[k].virtual_position))
 				$Area2D/CollisionShape2D.shape.points = polygon
+				break
+		if loop_found:
+			break
+	for i in range(num_chicks):
+		chicks[i].interpolation_factor = lerp(chicks[i].interpolation_factor, 10., 1. * delta)
+		chicks[i].node.position = lerp(chicks[i].node.position, chicks[i].virtual_position, chicks[i].interpolation_factor * delta)
 
 func move_towards_angle(current: float, target: float, delta: float) -> float:
 	var diff = wrapf(target - current, -PI, PI)
